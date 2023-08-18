12478cd4837bfe2c20b2ec39c0393e08a36177bb
PeiJun <peij2011@163.com>
2023-08-18 10:54:17 +0800

    test

--- sdwebui/flutter_service_worker.js
+++ sdwebui/flutter_service_worker.js
@@ -4,9 +4,10 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "36d50e0095a772f58ec2560e7f82bd35",
-"index.html": "fd71ab56e03b20c117755f878b405da4",
-"/": "fd71ab56e03b20c117755f878b405da4",
+"index.html": "32a46da2585f18b9874b1e8e8e5c40ef",
+"/": "32a46da2585f18b9874b1e8e8e5c40ef",
 "main.dart.js": "75c5339ed37ac02bd00d241be01b53d0",
+"flutter.js": "a85fcf6324d3c4d3ae3be1ae4931e9c5",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
@@ -21,7 +22,11 @@ const RESOURCES = {
 "assets/fonts/MaterialIcons-Regular.otf": "e7069dfd19b331be16bed984668fe080",
 "assets/assets/files/anime-words-getAll": "4d75f54e96fd7f69eeab0f6977b2ef1d",
 "assets/assets/files/anime-labels-getAll": "ff6bb1dc3953ddf3aaa9bf92b78d6611",
-"assets/assets/files/imageStyleList": "14ae86daa3a81fa53d23186328fa7ba3"
+"assets/assets/files/imageStyleList": "14ae86daa3a81fa53d23186328fa7ba3",
+"canvaskit/canvaskit.js": "97937cb4c2c2073c968525a3e08c86a3",
+"canvaskit/profiling/canvaskit.js": "c21852696bc1cc82e8894d851c01921a",
+"canvaskit/profiling/canvaskit.wasm": "371bc4e204443b0d5e774d64a046eb99",
+"canvaskit/canvaskit.wasm": "3de12d898ec208a5f31362cc00f09b9e"
 };
 
 // The application shell files that are downloaded before a service worker can
